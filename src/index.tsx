ae1ad1984f056b63a9a3c4b47c56bdc998af5984
Tyler Goelz <tyler@tylergoelz.com>
2025-06-20 11:31:16 -0700

    fix cra so the demo loads

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-export { ReactSpreadsheetImport } from "./ReactSpreadsheetImport"
+export { ReactSpreadsheetImport } from './ReactSpreadsheetImport';
+
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
