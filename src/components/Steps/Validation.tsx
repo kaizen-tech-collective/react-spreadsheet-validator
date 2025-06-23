3e4c6156ec8c65432c4a8620528e4315b9523894
Tyler Goelz <tyler@tylergoelz.com>
2025-06-23 16:19:23 -0700

    add 'show only rows with errors' feature

--- src/components/Steps/Validation.tsx
+++ src/components/Steps/Validation.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 import Box from '@mui/material/Box';
+import FormLabel from '@mui/material/FormLabel';
+import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 
 import { DataGrid, GridRowModel } from '@mui/x-data-grid';
@@ -18,15 +20,16 @@ type Props<T extends string> = {
 export const ValidationStep = <T extends string>({ initialData }: Props<T>) => {
   const { fields, rowHook, rtl, tableHook, translations } = useRsi<T>();
 
-  const [rows, setRows] = React.useState<(Data<T> & Meta)[]>(
+  const [data, setData] = React.useState<(Data<T> & Meta)[]>(
     React.useMemo(() => {
       return addErrorsAndRunHooks<T>(initialData, fields, rowHook, tableHook);
     }, []),
   );
+  const [filterByErrors, setFilterByErrors] = React.useState(false);
 
   const handleProcessRowUpdate = React.useCallback(
     (newRow: GridRowModel) => {
-      const rowIndex = rows.findIndex(row => {
+      const rowIndex = data.findIndex(row => {
         return row.__index === newRow.__index;
       });
 
@@ -34,25 +37,52 @@ export const ValidationStep = <T extends string>({ initialData }: Props<T>) => {
         return newRow;
       }
 
-      const updateData = [...rows];
-      updateData[rowIndex] = { ...rows[rowIndex], ...newRow };
+      const updateData = [...data];
+      updateData[rowIndex] = { ...data[rowIndex], ...newRow };
 
       const validated = addErrorsAndRunHooks<T>(updateData, fields, rowHook, tableHook);
-      setRows(validated);
+      setData(validated);
 
       return validated[rowIndex] ?? newRow;
     },
-    [rows, fields, rowHook, tableHook],
+    [data, fields, rowHook, tableHook],
   );
 
   const columns = React.useMemo(() => {
     return generateValidationStepColumns(fields);
   }, [fields]);
 
+  const rows = React.useMemo(() => {
+    if (filterByErrors) {
+      return data.filter(value => {
+        if (value?.__errors) {
+          return Object.values(value.__errors)?.filter(error => {
+            return error.level === 'error';
+          }).length;
+        }
+        return false;
+      });
+    }
+    return data;
+  }, [data, filterByErrors]);
+
   return (
     <>
       <Box display="flex" justifyContent="space-between" alignItems="center" mb="2rem" flexWrap="wrap" gap="8px">
         <Typography variant={'h4'}>{translations.validationStep.title}</Typography>
+        <Box display="flex" gap="16px" alignItems="center" flexWrap="wrap">
+          {/* <Button onClick={deleteSelectedRows} size={'small'} variant={'outlined'}>
+            {translations.validationStep.discardButtonTitle}
+          </Button> */}
+          <Switch
+            sx={{ display: 'flex', alignItems: 'center' }}
+            checked={filterByErrors}
+            onChange={() => {
+              return setFilterByErrors(!filterByErrors);
+            }}
+          />
+          <FormLabel component="legend">{translations.validationStep.filterSwitchTitle}</FormLabel>
+        </Box>
       </Box>
       <Box
         sx={{
