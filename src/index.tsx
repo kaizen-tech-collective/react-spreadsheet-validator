92f4c1a52118e6f1d5fc2fdf4b98f23cebdc7f88
Tyler Goelz <tyler@tylergoelz.com>
2025-06-24 13:52:30 -0700

    implement build process with rollup and publish to github npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-export { ReactSpreadsheetImport } from './ReactSpreadsheetImport';
-
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+export { ReactSpreadsheetImport } from './ReactSpreadsheetImport';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
